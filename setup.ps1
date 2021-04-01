08586c51b231dd002375d374e20c92fa7db3c3a7
thertgers <62949099+thertgers@users.noreply.github.com>
2021-04-02 01:47:58 +0200

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,5 +1,5 @@
 ﻿Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-choco install mysql-connector python git -y
+choco install mysql mysql-connector python git -y
 Start-Process "C:\tools\mysql\current\bin\mysql.exe" -ArgumentList '-u root -e "create schema notes"'
 py pip install django django-crispy-forms
 Start-Process "C:\Program Files\Git\cmd\git.exe" -ArgumentList 'clone https://github.com/thertgers/app.git'
