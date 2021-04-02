20086b6d39ead0683d54b2485755022a73f2b0b6
thertgers <62949099+thertgers@users.noreply.github.com>
2021-04-02 02:14:39 +0200

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -3,7 +3,7 @@ choco install mysql mysql-connector python git -y
 Start-Process "C:\tools\mysql\current\bin\mysql.exe" -ArgumentList '-u root -e "create schema notes"'
 py pip install django django-crispy-forms
 Start-Process "C:\Program Files\Git\cmd\git.exe" -ArgumentList 'clone https://github.com/thertgers/app.git'
-cd 'C:\Program Files\Git\app'
+cd $env:USERPROFILE\app
 py manage.py migrate
 py manage.py loaddata inital_data.json
 py manage.py runserver
